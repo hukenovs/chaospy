eee2d2378fa03b2892eb41c590fec599f4a0d4c1
capitanov <sallador@bk.ru>
2020-07-17 01:50:18 +0300

    Add plots

--- src/attractors/attractor.py
+++ src/attractors/attractor.py
@@ -43,8 +43,17 @@ OR CORRECTION.
 from abc import abstractmethod
 from typing import Tuple
 
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import kurtosis, skew
+
+CHARGS = {
+    "chua": {"alpha": 0.1, "beta": 28, "mu0": -1.143, "mu1": -0.714},
+    "duffing": {"a": 0.1, "b": 11},
+    "lorenz": {"sigma": 10, "beta": 8 / 3, "rho": 28},
+    "rikitake": {"a": 1, "mu": 1},
+    "rossler": {"a": 0.2, "b": 0.2, "c": 5.7},
+}
 
 
 class BaseAttractor:
@@ -88,7 +97,7 @@ class BaseAttractor:
 
     def reset_coordinates(self):
         """Reset coordinates to zeros: X, Y, Z = 0"""
-        self.coordinates = np.zeros(shape=(self.num_points, 3), dtype=float)
+        self.coordinates = None
 
     def _step_coordinates(self, coordinates: Tuple[float, float, float]):
         """Make generator for coordinates.
@@ -112,7 +121,6 @@ class BaseAttractor:
     @staticmethod
     def check_moments(coorditanes: np.array) -> dict:
         """Calculate stochastic parameters: mean, variance, skewness, kurtosis etc.
-
         """
         return {
             "Mean": np.mean(coorditanes, axis=0),
@@ -122,9 +130,29 @@ class BaseAttractor:
             "Median": np.median(coorditanes, axis=0),
         }
 
+    def check_probability(self):
+        pass
+        # TODO: Implement this method!
+
+    def show_time_plots(self, save_plots: bool = False):
+        """Plot 3D coordinates as time series."""
+        plt.figure("Coordinates evolution in time", figsize=(8, 6), dpi=100)
+        for ii, axis in enumerate(["X", "Y", "Z"]):
+            plt.subplot(3, 1, ii + 1)
+            plt.plot(self.coordinates[:, ii], linewidth=0.75)
+            plt.grid(True)
+            if axis == "Z":
+                plt.xlabel("Time")
+            plt.ylabel(axis)
+            plt.xlim([0, self.num_points - 1])
+        plt.tight_layout()
+        if save_plots:
+            plt.savefig(f"{self.__class__.__name__}_coordinates.png")
+        plt.show()
+
 
 if __name__ == "__main__":
-    chaotic_attractor = BaseAttractor(num_points=10, init_point=(1, 2, 3), step=10, nfft=1024)
+    chaotic_attractor = BaseAttractor(num_points=100, init_point=(0.1, 0.2, -0.1), step=10, nfft=1024)
     print(f"Start analyzing {chaotic_attractor.__class__.__name__} chaotic system: \n")
     chaotic_attractor.process_in_time()
     xyz = chaotic_attractor.coordinates
@@ -133,3 +161,5 @@ if __name__ == "__main__":
     moments = chaotic_attractor.check_moments(xyz)
     for key in moments:
         print(f"{key:<10}: {moments[key]}")
+
+    chaotic_attractor.show_time_plots(save_plots=False)
